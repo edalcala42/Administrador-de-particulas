8a18be6e5a6f7847a34a82ecb4dca6e326f825f3
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-10-22 16:04:44 -0500

    Se desarrolla el método to_dict para que los datos se guarden de acuerdo al orden JSON.

--- particula.py
+++ particula.py
@@ -1,8 +1,10 @@
 from algoritmos import distancia_euclidiana
 
 class Particula:
-    def __init__(self, id = 0, origen_x = 0, origen_y =0, destino_x = 0, destino_y = 0, velocidad = 0, red = 0,
-        green = 0, blue = 0, distancia = 0.0):
+    def __init__(
+        self, id = 0, origen_x = 0, origen_y =0, 
+        destino_x = 0, destino_y = 0, velocidad = 0,
+        red = 0, green = 0, blue = 0):
         self.__id = id
         self.__origen_x = origen_x
         self.__origen_y = origen_y
@@ -16,14 +18,27 @@ class Particula:
     
     def __str__(self):
         return(
-            'Id: ' + str(self.__id) + '\n' +
-            'Origen_x: ' + str(self.__origen_x) + '\n' +
-            'Origen_y: ' + str(self.__origen_y) + '\n' +
-            'Destino_x: ' + str(self.__destino_x) + '\n' +
-            'Destino_y: ' + str(self.__destino_y) + '\n' +
-            'Velocidad: ' + str(self.__velocidad) + '\n' +
-            'Red: ' + str(self.__red) + '\n' +
-            'Green: ' + str(self.__green) + '\n' +
-            'Blue: ' + str(self.__blue) + '\n' +
-            'Distancia: ' + str(self.__distancia) + '\n' 
+            'id: ' + str(self.__id) + '\n' +
+            'origen_x: ' + str(self.__origen_x) + '\n' +
+            'origen_y: ' + str(self.__origen_y) + '\n' +
+            'destino_x: ' + str(self.__destino_x) + '\n' +
+            'destino_y: ' + str(self.__destino_y) + '\n' +
+            'velocidad: ' + str(self.__velocidad) + '\n' +
+            'red: ' + str(self.__red) + '\n' +
+            'green: ' + str(self.__green) + '\n' +
+            'blue: ' + str(self.__blue) + '\n' +
+            'distancia: ' + str(self.__distancia) + '\n' 
         )
+
+    def to_dict(self):
+        return{
+            "id": self.__id,
+            "origen_x": self.__origen_x,
+            "origen_y": self.__origen_y,
+            "destino_x": self.__destino_x,
+            "destino_y": self.__destino_y,
+            "velocidad": self.__velocidad,
+            "red": self.__red,
+            "green": self.__green,
+            "blue": self.__blue,
+        }
